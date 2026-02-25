49616b90acef75b82edfd83a43529d0745b51c99
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-25 21:52:14 +0700

    3. constant

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
 
 int main() {
-    // 2. variables and basic data types
-
-    int x; //declaration of an integer variable named x
-    x = 5; //assignment of the value 5 to the variable x
-
-    int y = 6;
-    int sum = x + y;
-
-
-    std::cout << "x = " << x << '\n';
-    std::cout << "y = " << y << '\n';
-    std::cout << "sum = " << sum << '\n';
-
-    // integer (whole number): age, year (if put decimal -> truncated)
-    int age = 22;
-    // double (floating-point number): price, weight
-    double GPA = 4.00;
-    // char (single character): grade, initial | single quote
-    char grade = 'A';
-    // bool (boolean value): isStudent, isRaining
-    bool isStudent = true;
-    // string (text): name, city | double quote
-    std::string name = "Win.";
-
-    // output
-    std::cout << name;
+    // 3. Constant 
+    // The const keyword is used to declare a variable as constant, meaning its value cannot be changed after initialization.
+    // convention: upper-case declaration.
+    const int age = 22;
+    // age = 23; // This would cause a compilation error because age is a constant
+    std::cout << age;
     return 0;
 }
