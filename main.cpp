6fc0e781189c4013687c85528361582e70439941
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-25 21:46:10 +0700

    2. variables and basic data types.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello C++!!!!!!!!!!!!!!\n"; //vdo 1: C++ tutorial for beginners
+    // 2. variables and basic data types
+
+    int x; //declaration of an integer variable named x
+    x = 5; //assignment of the value 5 to the variable x
+
+    int y = 6;
+    int sum = x + y;
+
+
+    std::cout << "x = " << x << '\n';
+    std::cout << "y = " << y << '\n';
+    std::cout << "sum = " << sum << '\n';
+
+    // integer (whole number): age, year (if put decimal -> truncated)
+    int age = 22;
+    // double (floating-point number): price, weight
+    double GPA = 4.00;
+    // char (single character): grade, initial | single quote
+    char grade = 'A';
+    // bool (boolean value): isStudent, isRaining
+    bool isStudent = true;
+    // string (text): name, city | double quote
+    std::string name = "Win.";
+
+    // output
+    std::cout << name;
     return 0;
 }
