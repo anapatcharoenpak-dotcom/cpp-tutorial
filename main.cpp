20061bb84960d1d24dd1b5aca48d50526a9e1caa
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-25 21:09:11 +0700

    1_tutorial for beginner

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello C++!!!!!!!!!!!!!!\n";
+    std::cout << "Hello C++!!!!!!!!!!!!!!\n"; //vdo 1: C++ tutorial for beginners
     return 0;
 }
