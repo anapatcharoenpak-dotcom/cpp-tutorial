e057bfc3b9f9c2b12588e7709f3f741f8b125cd3
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-26 16:10:18 +0700

    7. Type conversion

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 #include <vector>
 
 int main (){
-    int students = 20;
-    students += 1; // or students++ (for +1)
-    //students -= 1; // or students-- (for -1)
-    //students *= 2; (multiply by 2)
-    //students /=2; (divide by 2)
-    int remainder = students % 2;
-    std::cout << students << '\n';
-    std::cout << remainder;
+    // 7. type conversion = conversion of a value of one data type to another
+    // 7.1 implicit type conversion = automatic type conversion performed by the compiler
+    // 7.2 explicit type conversion = manual type conversion performed by the programmer using casting
+
+    double x = (int) 3.14;
+    std::cout << x << "\n"; // explicit cast
+
+    char y = 100;
+    std::cout << y << "\n"; // explicit cast -> convert according to ASCII table
+
+    // why do we use this
+
+    int correct = 8;
+    int questions = 10;
+    double score = correct/(double)questions * 100; // note: questions doesn't permanently change to double type for every other line!
+    std::cout << score << " %";
+
     return 0;
 }
