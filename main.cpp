4e292de58fd86c09308356474e1ec6b3e63a9f4b
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-26 16:42:38 +0700

    9. Useful Math Function

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 int main(){
-    // 8. Character and String Input.
-    std::string name;
-    std::string fullName;
-
-    std::cout << "What's your name?: ";
-    std::getline(std::cin, name);
-
-    std::cout << "What's your fullname?: ";
-    std::getline(std::cin, fullName); // std::cin with getline() is avoided because it can cause issues with input buffering.
-
-    std::cout << "Hello " << name << " " << fullName;
-
+    // 9. Useful math function
+    double z;
+    z = sqrt(9);
+    z = pow(2, 3);
+    std::cout << z;
     return 0;
 }
