d74c73f480fbd747f1651772d5dda82704e741c1
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-26 16:32:07 +0700

    8. Character Input

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
-int main (){
-    // 7. type conversion = conversion of a value of one data type to another
-    // 7.1 implicit type conversion = automatic type conversion performed by the compiler
-    // 7.2 explicit type conversion = manual type conversion performed by the programmer using casting
+int main(){
+    // 8. Character and String Input.
+    std::string name;
+    std::string fullName;
 
-    double x = (int) 3.14;
-    std::cout << x << "\n"; // explicit cast
+    std::cout << "What's your name?: ";
+    std::getline(std::cin, name);
 
-    char y = 100;
-    std::cout << y << "\n"; // explicit cast -> convert according to ASCII table
+    std::cout << "What's your fullname?: ";
+    std::getline(std::cin, fullName);
 
-    // why do we use this
-
-    int correct = 8;
-    int questions = 10;
-    double score = correct/(double)questions * 100; // note: questions doesn't permanently change to double type for every other line!
-    std::cout << score << " %";
+    std::cout << "Hello " << name << " " << fullName;
 
     return 0;
 }
