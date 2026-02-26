c83486365ce32459dec5b0838dc9bec0c4652bb7
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-26 16:46:18 +0700

    10. (exercise 1) hypothenus

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <cmath>
 
 int main(){
-    // 9. Useful math function
-    double z;
-    z = sqrt(9);
-    z = pow(2, 3);
-    std::cout << z;
+    // 10. Exercise 1: Calculating Hypothenus of right triangle.
+    double a, b;
+    std::cout << "Enter one side of right triangle a: ";
+    std::cin >> a;
+
+    std::cout << "Enter one side of right triangle b: ";
+    std::cin >> b;
+
+    double c = sqrt(pow(a, 2) + pow (b, 2));
+    std::cout << "Hypothenus is " << c;
     return 0;
 }
