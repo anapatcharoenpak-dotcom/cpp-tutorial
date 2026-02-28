69b5ae17e6317a8b178ecc740e2f0933858ff91a
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-28 16:18:03 +0700

    18. While loop

--- main.cpp
+++ main.cpp
@@ -2,23 +2,14 @@
 #include <cmath>
 
 int main(){
-    // 11. If statement - easy (already learnt)
-    // 12. Switch statement - easy (already learnt)
-    // 13. calculator
-    // 14. ternary operator: condition ? expression1 (true) : expression 2 (false); 
-    // 15. logical operator:
-    // && (and), || (or), ! (negation)
-    // 16. (exercise 2): temperature conversion.
-    // 17. Useful String Method - at, append, insert, find
-
+    // 18: while loop
     std::string name;
-
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
+  
+    while(name.empty()){
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
     
-    std::cout << name.append("@gmail.com");
-
-    std::cout << "Your username is now: " << name;
-    
+    std::cout << "Hello " << name;
     return 0;
 }
