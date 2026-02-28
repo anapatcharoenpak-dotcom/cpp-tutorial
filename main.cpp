a42bb6f791c80c64bd459889396fa662ea9145f6
Anapat Charoenpak <anapatcharoenpak@gmail.com>
2026-02-28 16:34:38 +0700

    21.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,23 @@
 #include <cmath>
 
 int main(){
-    // 19: do while loop - do once if true then do again 
-    int num;
+    // 20. for loop
+    
+    for(int i = 1; i <= 20; i+=1){
 
-    do{
-        std::cout << "Enter a position number: ";
-        std::cin >> num;
-    }while(num < 0);
+        if(i == 13){
+            continue;
+        }
+        std::cout << i << '\n';
 
-    std::cout << "The number is " << num;
+    }
+
+    std::cout << "Yay";
+
+
+    // 21. break and continue
+    // break = break out of a loop
+    // continue = skip current iteration
+    
+    
 }
